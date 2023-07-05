bbd4943269d58018033c2ba521cb7abfb78bf846
lmelkamu <lydiamelkamu24@gmail.com>
2023-07-05 21:04:50 +0000

    dither :(

--- src/blur.ml
+++ src/blur.ml
@@ -1,8 +1,22 @@
 open Core
 
-(* You need to modify this function to blur the input image
-   based on the provided radius instead of ignoring it. *)
-let transform image ~radius:_ = image
+(* You need to modify this function to blur the input image based on the
+   provided radius instead of ignoring it. *)
+let transform image ~radius =
+  Image.mapi image ~f:(fun ~x ~y _pixel ->
+    let x_start = if x - 1 > 0 then 0 else x - radius in
+    let x_end =
+      if x + 1 > Image.width image then Image.width image else x + radius
+    in
+    let y_start = if y - radius < 0 then 0 else y - radius in
+    let y_end =
+      if y + radius > Image.height image
+      then Image.height image
+      else y + radius
+    in
+    let section = Image.slice image ~x_start ~x_end ~y_start ~y_end in
+    Image.mean_pixel section)
+;;
 
 let command =
   Command.basic
@@ -24,5 +38,6 @@ let command =
         let image' = transform image ~radius in
         Image.save_ppm
           image'
-          ~filename:(String.chop_suffix_exn filename ~suffix:".ppm" ^ "_blur.ppm")]
+          ~filename:
+            (String.chop_suffix_exn filename ~suffix:".ppm" ^ "_blur.ppm")]
 ;;
--- src/dither.ml
+++ src/dither.ml
@@ -1,8 +1,23 @@
 open Core
 
 (* This should look familiar by now! *)
-let transform image = image
+let transform image = 
+  Image.foldi image ~init:image ~f:(fun ~x ~y image (r,_g,_b) ->
+    let max = Image.max_val image in 
+    let value = (Int.to_float r)/.(Int.to_float max) in
+    
+    let error = if value > 0.5
+       then (Image.set image ~x ~y (max,max,max);
+    (r - max)) 
+    else ((Image.set image ~x ~y (0,0,0); r)) in
 
+    (* if x+1 < Image.width image then let new_val = (value + (7/16)*error)*max in Image.set image ~x:x+1 ~y (new_val,new_val,new_val) in 
+    if (x-1 > 0 && y+1 <Image.height image) then let new_val = ((value + 3/16*error))*max in Image.set image ~x:x-1 ~y:y+1 (new_val,new_val,new_val) in 
+    if (x+1 < max && y+1 <Image.height image) then let new_val = ((value + 5/16*error))*max in Image.set image ~x:x+1 ~y (new_val,new_val,new_val) in 
+    if y+1 < Image.height image then let new_val = ((value + 1/16*error))*max in Image.set image ~x:x+1 ~y (new_val,new_val,new_val) in 
+    new_pixel  *)
+  )
+;;
 let command =
   Command.basic
     ~summary:"Dither an image"
